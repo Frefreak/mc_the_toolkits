33ec65ea269b02d37865d45b5d52deb4dfde1d7a
Xiangyu Zhu <frefreak.zxy@gmail.com>
2023-01-30 18:05:43 +0800

    fix key detection

--- src/main/java/nz/carso/the_toolkits/TheToolkitsEventHandler.java
+++ src/main/java/nz/carso/the_toolkits/TheToolkitsEventHandler.java
@@ -19,10 +19,10 @@ import static org.lwjgl.glfw.GLFW.GLFW_KEY_SLASH;
 
 public class TheToolkitsEventHandler {
     private static final Logger LOGGER = LogUtils.getLogger();
-    static KeyMapping whatKey = new KeyMapping("key." + Constants.MOD_ID + ".whatkey", KeyConflictContext.GUI, KeyModifier.SHIFT, InputConstants.Type.KEYSYM, GLFW_KEY_SLASH, "key.categories." + Constants.MOD_ID);
+    static KeyMapping linkKey = new KeyMapping("key." + Constants.MOD_ID + ".link_key", KeyConflictContext.GUI, KeyModifier.SHIFT, InputConstants.Type.KEYSYM, GLFW_KEY_SLASH, "key.categories." + Constants.MOD_ID);
 
     public static void init() {
-        ClientRegistry.registerKeyBinding(whatKey);
+        ClientRegistry.registerKeyBinding(linkKey);
     }
 
 
@@ -31,7 +31,7 @@ public class TheToolkitsEventHandler {
         @SubscribeEvent
         public static void keyPress(ScreenEvent.KeyboardKeyPressedEvent.Post event)
         {
-            if (!whatKey.consumeClick()) {
+            if (!linkKey.isActiveAndMatches(InputConstants.getKey(event.getKeyCode(), event.getScanCode()))) {
                 return;
             }
             if (TheToolkits.isJEIAvailable()) {
